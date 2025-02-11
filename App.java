7f4c68c5c22e01c97c346e5fdcc455ce99f664c2
Ayla Lopes <163359268+taengooni@users.noreply.github.com>
2025-02-11 22:19:11 +0000

    oi

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main  (String[] args) throws Exception {
+        char gender = 'f';
+        String name = "ayla";
+        int age = 19;
+        double height = 1.65;
+     System.out.printIn("Nome: " + name);
+     System.out.printIn("Sua idade é: " + age);
+     System.out.printIn("Sua altura é: " + height);
+     
+    }
+}
