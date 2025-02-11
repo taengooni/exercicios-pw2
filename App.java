2f64d9889f8637eeb199a247d9a6f6d70043c0a8
Ayla Lopes <163359268+taengooni@users.noreply.github.com>
2025-02-11 22:40:57 +0000

    oi

--- App.java
+++ App.java
@@ -4,9 +4,19 @@ public class App {
         String name = "ayla";
         int age = 19;
         double height = 1.65;
-     System.out.printIn("Nome: " + name);
-     System.out.printIn("Sua idade é: " + age);
-     System.out.printIn("Sua altura é: " + height);
-     
+     System.out.println("Nome: " + name);
+     System.out.println("Sua idade é: " + age);
+     System.out.println("Sua altura é: " + height);
+
+    if (age < 16){
+        System.out.println(name + "não pode voltar");
     }
+    else if (age < 18 || age > 64){
+        System.out.println(name + "pode votar (facultativo)")
+    }
+    else {
+        System.out.println(name + "tem que votar obrigatoriamente");
+
+    }
+}
 }
